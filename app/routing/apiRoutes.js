0c91322f5ee189fad97bdcfb962586062fa710ac
Cameron Elliott <CameronElliott@Camerons-MacBook-Pro-4.local>
2018-04-30 17:29:17 -0700

    edit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,23 +29,21 @@ app.post("/api/friends", function(req, res) {
 
     var totalDifference = 0; 
 
-		// Loop through friend database
+	
 		for  (var i=0; i< friendData.length; i++) {
 			console.log("frienddata.length" + friendData.length)
 
 			console.log(friendData[i].name);
 			totalDifference = 0;
 
-			// Loop through their scores
+
 			for (var j=0; j< friendData[i].scores[j]; j++){
 				console.log("frienddatascores" + friendData[i].scores[j]);
 
-				// We calculate the difference between the scores and sum them into the totalDifference
 
 				totalDifference += Math.abs(parseInt(newFScores[j]) - parseInt(friendData[i].scores[j]));
 				console.log("total difference" + totalDifference);
 
-				// If the sum of differences is less then the differences of the current "best match"
 				if (totalDifference <= matchUser.diff){
 
 					// Reset the matchUser
